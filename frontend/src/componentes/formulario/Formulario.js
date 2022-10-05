97ab3fdfdeb3fb958a327ef23db10a9c1965eea4
JimenaKatuMartinez <93katu@gmail.com>
2022-10-05 13:48:07 -0300

    Inicio componentes inputs

---
+++ frontend/src/componentes/formulario/Formulario.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Inputs } from '../inputs/Inputs';
+
+const Formulario = () => {
+  return (
+    <div>
+      <Inputs/>
+    </div>
+  );
+};
+
+export default Formulario;
---
+++ frontend/src/componentes/inputs/Inputs.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const WrapperInput = styled.div`
+    
+`;
+
+export const Inputs = () => {
+  return (
+    <WrapperInput>Inputs</WrapperInput>
+  );
+};
+
+export const DataList = () => {
+  return (
+      <div>Inputs</div>
+  );
+};
+
+export const RadioButton = () => {
+  return (
+    <div>Inputs</div>
+  );
+};

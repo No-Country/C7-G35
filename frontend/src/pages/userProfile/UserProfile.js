c73fb9cf52e92ffa71e60b3f5ce7e3a5fdf99220
JimenaKatuMartinez <93katu@gmail.com>
2022-10-16 21:35:06 -0300

    refactor(frontend): botones en perfil user
    Agregasdos botones para registrar mascotas

--- frontend/src/pages/userProfile/UserProfile.js
+++ frontend/src/pages/userProfile/UserProfile.js
@@ -6,56 +6,69 @@ import CardMascota from '../../componentes/cardMascota/CardMascota';
 import { SVGWavesSuperior } from '../../componentes/SVGWaves/SVGWaves';
 import useFetch from '../../customHooks/useFetch';
 import SinFotoMascota from '../../assets/sinFotoMascota.jpg';
+import { ButtonComponent, ButtonComponentShort } from '../../componentes/buttom/Button';
 
 const WrapperUserProfile = styled.div`
-    color: var(--clr-blue-dark);
+  color: var(--clr-blue-dark);
 `;
 
 const WrapperSaludo = styled.div`
-    background-color: var(--clr-pink);
-    padding: 2rem;
-    `;
+  background-color: var(--clr-pink);
+  padding: 2rem;
+`;
 
 const Saludo = styled.p`
-    font-size: 2rem;
-    `;
+  font-size: 2rem;
+`;
 
 const UserName = styled.h2`
-    font-size: 3.5rem;
-    `;
+  font-size: 3.5rem;
+`;
+
+const WrapperButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 const WrapperTodasLasMascotas = styled.div`
-    padding: 1rem;
-    `;
+  padding: 1rem;
+`;
+
+const WapperInfoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const WrapperMascotasRegistradas = styled.div`
-    margin-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
 `;
 
 const Titulo = styled.h3`
-    padding-left: 0.5rem;
-    margin-bottom: .5rem;
-    border-left: 1rem solid var(--clr-blue-dark);
-    border-radius: 5px;
+  padding-left: 0.5rem;
+  margin-bottom: 0.5rem;
+  border-left: 1rem solid var(--clr-blue-dark);
+  border-radius: 5px;
 `;
 
 const Descripcion = styled.p`
-    margin-left: .5rem;
-    color: var(--clr-grey-dark);
+  margin-left: 0.5rem;
+  margin-bottom: .5rem;
+  color: var(--clr-grey-dark);
 `;
 
 const DescripcionSinRegistro = styled.p`
-    padding: 1rem;
-    color: var(--clr-grey-dark);
-    background-color:var(--clr-grey-medium) ;
+  padding: 1rem;
+  color: var(--clr-grey-dark);
+  background-color: var(--clr-grey-medium);
 `;
 
 const WrapperListadoCards = styled.div`
-    padding: 1rem;
-    display: flex;
-    gap: 1rem;
-    flex-wrap: wrap;
-    justify-content: center;
+  padding: 1rem;
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 const UserProfile = () => {
@@ -87,70 +100,91 @@ const UserProfile = () => {
       }
     });
 
-    axios.delete(
-      `http://localhost:8000/api/pets/${id}`,
-      { headers: { Authorization: `Bearer ${token}` } },
-    );
+    axios.delete(`http://localhost:8000/api/pets/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
   };
   return (
     <WrapperUserProfile>
-        <WrapperSaludo>
-            <Saludo>Hola </Saludo>
-            <UserName>{userName}!</UserName>
-        </WrapperSaludo>
-        <SVGWavesSuperior/>
-        <WrapperTodasLasMascotas>
-            <WrapperMascotasRegistradas>
+      <WrapperSaludo>
+        <Saludo>Hola </Saludo>
+        <UserName>{userName}!</UserName>
+        <WrapperButtons>
+          <ButtonComponent
+            texto={'Encontré una mascota'}
+            estado={'Encontrado'}
+            path={'/form-add-found-pet'}
+          />
+          <ButtonComponent
+            texto={'Busco mi mascota'}
+            path={'/form-add-lost-pet'}
+          />
+        </WrapperButtons>
+      </WrapperSaludo>
+      <SVGWavesSuperior />
+      <WrapperTodasLasMascotas>
+        <WrapperMascotasRegistradas>
+            <WapperInfoSection>
                 <Titulo>Tus mascotas registradas</Titulo>
-                <Descripcion>Aquí podrás ver las mascotas que subiste al sitio</Descripcion>
-                <WrapperListadoCards>
-                   {MascotasRegistradas
-                     ? MascotasRegistradasData?.map((mascota) => (
-                        <CardMascota
-                          key={mascota?.id}
-                          id={'/detail-pet'}
-                          nombre={mascota?.name}
-                          link={mascota?.images ? mascota?.images[0] : SinFotoMascota}
-                          fecha={mascota?.date}
-                          estado={mascota?.estado}
-                          token={token}
-                          deleteFunction={() => handleDelete(mascota?.id) }
-                        />
-                     ))
-                     : <DescripcionSinRegistro>
-                    Veo que no registraste ninguna mascota,
-                    tal vez podrías considerar hacerlo, por
-                    precaución. Si tu mascota se pierde, rápidamente
-                    puedes marcarla como &quot;perdida&quot; para agilizar la búsqueda.
-                     No todos los datos son obligatorios, pero mientras mas completo este
-                     el formulario, mas posibilidades tienes de encontrarlo si se pierda.
-                     <Link to='/'>Registra tu mascota aquí</Link>
-                    </DescripcionSinRegistro> }
-                </WrapperListadoCards>
-            </WrapperMascotasRegistradas>
-            <WrapperMascotasRegistradas>
-                <Titulo>Tu mascota perdida</Titulo>
-                <Descripcion>Aquí verás la mascota marcada &quot;perdida&quot;</Descripcion>
-                <WrapperListadoCards>
-                {MascotasPerdidas
-                  ? MascotasPerdidasData?.map((mascota) => (
-                        <CardMascota
-                          key={mascota?.id}
-                          id={'/detail-pet'}
-                          nombre={mascota?.name}
-                          link={mascota?.images ? mascota?.images[0] : SinFotoMascota}
-                          fecha={mascota?.date}
-                          estado={mascota?.estado}
-                          token={token}
-                          deleteFunction={() => handleDelete(mascota?.id) }
-                        />
-                  ))
-                  : <DescripcionSinRegistro>
-                    No tienes ninguna mascota marcada como perdida, que bueno!
-                    </DescripcionSinRegistro> }
-                </WrapperListadoCards>
-            </WrapperMascotasRegistradas>
-        </WrapperTodasLasMascotas>
+                <Descripcion>
+                     Aquí podrás ver las mascotas que subiste al sitio
+                </Descripcion>
+                <ButtonComponentShort as={'a'} texto={'Agregar otra mascota'} path={'/form-add-pet'}/>
+            </WapperInfoSection>
+          <WrapperListadoCards>
+            {MascotasRegistradas ? (
+              MascotasRegistradasData?.map((mascota) => (
+                <CardMascota
+                  key={mascota?.id}
+                  id={'/detail-pet'}
+                  nombre={mascota?.name}
+                  link={mascota?.images ? mascota?.images[0] : SinFotoMascota}
+                  fecha={mascota?.date}
+                  estado={mascota?.estado}
+                  token={token}
+                  deleteFunction={() => handleDelete(mascota?.id)}
+                />
+              ))
+            ) : (
+              <DescripcionSinRegistro>
+                Veo que no registraste ninguna mascota, tal vez podrías
+                considerar hacerlo, por precaución. Si tu mascota se pierde,
+                rápidamente puedes marcarla como &quot;perdida&quot; para
+                agilizar la búsqueda. No todos los datos son obligatorios, pero
+                mientras mas completo este el formulario, mas posibilidades
+                tienes de encontrarlo si se pierda.
+                <Link to='/'>Registra tu mascota aquí</Link>
+              </DescripcionSinRegistro>
+            )}
+          </WrapperListadoCards>
+        </WrapperMascotasRegistradas>
+        <WrapperMascotasRegistradas>
+          <Titulo>Tu mascota perdida</Titulo>
+          <Descripcion>
+            Aquí verás la mascota marcada &quot;perdida&quot;
+          </Descripcion>
+          <WrapperListadoCards>
+            {MascotasPerdidas ? (
+              MascotasPerdidasData?.map((mascota) => (
+                <CardMascota
+                  key={mascota?.id}
+                  id={'/detail-pet'}
+                  nombre={mascota?.name}
+                  link={mascota?.images ? mascota?.images[0] : SinFotoMascota}
+                  fecha={mascota?.date}
+                  estado={mascota?.estado}
+                  token={token}
+                  deleteFunction={() => handleDelete(mascota?.id)}
+                />
+              ))
+            ) : (
+              <DescripcionSinRegistro>
+                No tienes ninguna mascota marcada como perdida, que bueno!
+              </DescripcionSinRegistro>
+            )}
+          </WrapperListadoCards>
+        </WrapperMascotasRegistradas>
+      </WrapperTodasLasMascotas>
     </WrapperUserProfile>
   );
 };

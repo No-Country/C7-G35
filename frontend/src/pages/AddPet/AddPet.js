aca1f47d7fd25c3c5ca81b3ccd8d27c817e52c1e
JimenaKatuMartinez <93katu@gmail.com>
2022-10-16 21:51:18 -0300

    refactor(frontend): agregado forumlario para registrar mascota

--- frontend/src/pages/AddPet/AddPet.js
+++ frontend/src/pages/AddPet/AddPet.js
@@ -3,27 +3,20 @@ import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { useNavigate } from 'react-router-dom';
+import 'yup-phone';
+import { json, useNavigate } from 'react-router-dom';
 import {
   RiCheckboxCircleFill,
   RiCheckboxBlankCircleLine,
 } from 'react-icons/ri';
 import axios from 'axios';
-import { useCookies } from 'react-cookie';
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
-} from 'react-leaflet';
-import L from 'leaflet';
-
 import {
   CheckboxComponent,
   CheckboxComponente,
   DataListComponent,
+  Error,
   InputTextComponent,
+  MensajeAclaracion,
   RadioButtonIconComponent,
   TextAreaComponent,
 } from '../../componentes/inputs/Inputs';
@@ -44,30 +37,6 @@ import { ButtonComponent } from '../../componentes/buttom/Button';
 import { mascotaTamanio } from '../../helpers/Tamaño';
 import { colores } from '../../helpers/Colores';
 
-function LocationMarker({ setPos, handleChange }) {
-  const [position, setPosition] = useState(null);
-
-  const map = useMapEvents({
-    click(e) {
-      setPosition(e.latlng);
-      handleChange(e.latlng);
-      setPos(e.latlng);
-    },
-  });
-
-  return position === null ? null : (
-    <Marker
-      icon={L.icon({
-        iconUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-        iconSize: [25, 35],
-      })}
-      position={position}
-    >
-      <Popup>Fue por aquí!</Popup>
-    </Marker>
-  );
-}
-
 const WrapperMascotaPerdida = styled.form`
   display: flex;
   flex-direction: column;
@@ -78,16 +47,19 @@ const WrapperMascotaPerdida = styled.form`
 
 const schemaAddLostPet = yup
   .object({
-    type: yup.string(),
-    gender: yup.string(),
-    size: yup.string(),
-    color: yup.array().ensure(),
-    breed: yup.string(),
+    type: yup.string('Este campo es requerido').nullable().required('Este campo es requerido'),
+    gender: yup.string().nullable().required('Este campo es requerido'),
+    size: yup.string().nullable().required('Este campo es requerido'),
+    color: yup.array().ensure().max('4', 'Elija 4 colores como máximo'),
+    breed: yup.string().required('Este campo es requerido').nullable(),
     location: yup.string(),
     age: yup.string(),
     isCastrated: yup.boolean(),
     name: yup.string().max(30, 'Ingresa como máximo 30 caractéres.'),
     description: yup.string().max(250, 'Ingresa como máximo 250 caractéres.'),
+    date: yup.string(),
+    mobile: yup.string().phone().nullable(),
+    email: yup.string().email().required('Este campo es requerido'),
   })
   .required();
 
@@ -102,39 +74,22 @@ const AddPet = () => {
     resolver: yupResolver(schemaAddLostPet),
   });
 
-  const [cookies, setCookie] = useCookies(['token']);
-  function onChange(newName) {
-    setCookie('name', newName, { path: '/' });
-  }
+  const { token } = JSON.parse(localStorage.getItem('token'));
 
   const navigate = useNavigate();
+  const useFormChange = useFormChangeContext();
+  const useFormData = useFormContext();
   const handleAddMascota = async (data) => {
-    console.log(data);
-    await axios.post('http://localhost:8000/api/pets', data, {
-      headers: { Authorization: `Bearer ${cookies.token}` },
-    });
+    const response = await axios.post('http://localhost:8000/api/pets', data, { headers: { Authorization: `Bearer ${token}` } });
+    useFormChange((prevState) => ({
+      ...prevState,
+      id: response?.data?.petLoss?.pet?.id,
+    }));
+
     reset();
     navigate('/add-photo');
   };
 
-  const [pets, setPets] = useState('');
-
-  useEffect(() => {
-    const getMascotas = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/pets', {
-          headers: { Authorization: `Bearer ${cookies.token}` },
-        });
-        setPets(response);
-      } catch (error) {
-        console.log('messaje', error);
-      }
-    };
-    getMascotas();
-  }, []);
-  console.log(pets);
-
-  const useFormChange = useFormChangeContext();
   useEffect(() => {
     useFormChange((prevState) => ({
       ...prevState,
@@ -142,7 +97,6 @@ const AddPet = () => {
     }));
   }, [watch('type')]);
 
-  const useFormData = useFormContext();
   const { type } = useFormData;
 
   const [castrdoEsterilizada, setCastrdoEsterilizada] = useState('');
@@ -163,34 +117,13 @@ const AddPet = () => {
     }
   }, [watch('isCastrated')]);
 
-  async function getCity(latitude, longitud) {
-    const response = await axios.get(`https://us1.locationiq.com/v1/reverse.php?key=pk.90e4cbe0aae8a090aeae84bd1a0a9ee3&lat=${latitude}&lon=${longitud}&format=json`);
-    console.log(response?.data?.address);
-  }
-
-  const handleChangePoint = (coord) => {
-    console.log('data', coord);
-    getCity(coord.lat, coord.lng);
-  };
-
   return (
-    <WrapperMascotaPerdida onSubmit={handleSubmit(handleAddMascota)} onChange={onChange}>
-      <h2>Solo para registrar mascotas por prevencion</h2>
-      <MapContainer
-        style={{ height: '600px', width: '600px' }}
-        center={[-38.169114135560854, -65.75208708742923]}
-        zoom={5}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap"</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <LocationMarker handleChange={handleChangePoint}/>
-      </MapContainer>
-
+    <WrapperMascotaPerdida
+      onSubmit={handleSubmit(handleAddMascota)}
+    >
+      <h2>Registra tu mascota perdida, los primeros 4 puntos son obligatorios</h2>
       <WrapperComponentForm>
-        <TituloForm>Se perdio mi...</TituloForm>
+        <TituloForm>Mi mascota es un...</TituloForm>
         <OptionGroups>
           {mascotaTipoOpciones?.map((dato, index) => (
             <RadioButtonIconComponent
@@ -204,6 +137,7 @@ const AddPet = () => {
             />
           ))}
         </OptionGroups>
+        {errors.type && <Error text={errors.type.message}/>}
       </WrapperComponentForm>
 
       <WrapperComponentForm>
@@ -221,6 +155,35 @@ const AddPet = () => {
             />
           ))}
         </OptionGroups>
+        {errors.gender && <Error text={errors.gender.message}/>}
+      </WrapperComponentForm>
+
+      <WrapperComponentForm>
+        <TituloForm>Raza..</TituloForm>
+        <DataListComponent
+          Array={razasLista}
+          tipo={type}
+          validacion={{ ...register('breed') }}
+        />
+        {errors.breed && <Error text={errors.breed.message}/>}
+      </WrapperComponentForm>
+
+      <WrapperComponentForm>
+        <TituloForm>{castrdoEsterilizada}</TituloForm>
+        <MensajeAclaracion text={'*Si la respuesta es negativa, deje el campo como está.'}/>
+        <CheckboxComponente
+          icon={
+            watch('isCastrated') ? (
+              <RiCheckboxCircleFill />
+            ) : (
+              <RiCheckboxBlankCircleLine />
+            )
+          }
+          idFor={'isCastratedLost'}
+          label={isCastrated}
+          validacion={{ ...register('isCastrated') }}
+        />
+        {errors.isCastrated && <Error text={errors.isCastrated.message}/>}
       </WrapperComponentForm>
 
       <WrapperComponentForm>
@@ -242,6 +205,9 @@ const AddPet = () => {
 
       <WrapperComponentForm>
         <TituloForm>Color..</TituloForm>
+        <MensajeAclaracion text={'*Máximo 4 colores'} />
+        <MensajeAclaracion text={'*Elija los mas parecidos'} />
+        <MensajeAclaracion text={'*No se fije en el tipo de pelaje'} />
         <OptionGroups>
           {colores?.map((dato, index) => (
             <CheckboxComponent
@@ -255,15 +221,8 @@ const AddPet = () => {
             />
           ))}
         </OptionGroups>
-      </WrapperComponentForm>
+        {errors.color && <Error text={errors.color.message}/>}
 
-      <WrapperComponentForm>
-        <TituloForm>Raza..</TituloForm>
-        <DataListComponent
-          Array={razasLista}
-          tipo={type}
-          validacion={{ ...register('breed') }}
-        />
       </WrapperComponentForm>
 
       <WrapperComponentForm>
@@ -284,26 +243,11 @@ const AddPet = () => {
       </WrapperComponentForm>
 
       <WrapperComponentForm>
-        <TituloForm>{castrdoEsterilizada}</TituloForm>
-        <CheckboxComponente
-          icon={
-            watch('isCastrated') ? (
-              <RiCheckboxCircleFill />
-            ) : (
-              <RiCheckboxBlankCircleLine />
-            )
-          }
-          idFor={'isCastratedLost'}
-          label={isCastrated}
-          validacion={{ ...register('isCastrated') }}
-        />
-      </WrapperComponentForm>
-
-      <WrapperComponentForm>
         <InputTextComponent
           label={'Responde al nombre de...'}
           idFor={'nameLostPet'}
           validacion={{ ...register('name') }}
+          type={'text'}
         />
       </WrapperComponentForm>
 
@@ -315,6 +259,27 @@ const AddPet = () => {
           }
           idFor={'descriptionLostPet'}
           validacion={{ ...register('description') }}
+        />
+      </WrapperComponentForm>
+
+      <WrapperComponentForm>
+        <TituloForm>Mis datos de contacto son...</TituloForm>
+        <InputTextComponent
+          label={'Teléfono'}
+          idFor={'telOwnerLostPet'}
+          validacion={{ ...register('mobile') }}
+          type={'tel'}
+          orientacion={'horizontal'}
+          placeholder={'+54 3556677441'}
+        />
+        {errors.mobile && <Error text={errors.mobile.message}/>}
+         <InputTextComponent
+          label={'E-mail'}
+          idFor={'emailOwnerLostPet'}
+          validacion={{ ...register('email') }}
+          type={'email'}
+          orientacion={'horizontal'}
+          placeholder={'ejemplo@gmail.com'}
         />
       </WrapperComponentForm>
 

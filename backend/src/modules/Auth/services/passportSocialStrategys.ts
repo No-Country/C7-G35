0dc4251bcb9b2b5985a618a682831e85298f1cd3
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-10-20 17:54:23 -0400

    fix(backend): bud of redirectu url for Auth

--- backend/src/modules/Auth/services/passportSocialStrategys.ts
+++ backend/src/modules/Auth/services/passportSocialStrategys.ts
@@ -3,11 +3,13 @@ import { configEnv } from '../config';
 import { UserFromSocials } from '../types';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
 
+const apiHomeURL = configEnv.backendHomeUrl;
+
 export const googleStrategy = new GoogleStrategy(
   {
     clientID: configEnv.googleCredentials.clientID,
     clientSecret: configEnv.googleCredentials.clientSecret,
-    callbackURL: '/api/oauth2/redirect/google',
+    callbackURL: `${apiHomeURL}/api/oauth2/redirect/google` || '/api/oauth2/redirect/google',
     scope: ['profile', 'email']
   },
   function (accessToken, refreshToken, profile, cb) {
@@ -26,7 +28,7 @@ export const facebookStrategy = new FacebookStrategy(
   {
     clientID: configEnv.facebookCredentials.appID,
     clientSecret: configEnv.facebookCredentials.appSecret,
-    callbackURL: '/api/oauth2/redirect/facebook',
+    callbackURL: `${apiHomeURL}/api/oauth2/redirect/facebook` || '/api/oauth2/redirect/facebook',
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
   function (accessToken, refreshToken, profile, cb) {
